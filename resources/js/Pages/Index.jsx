ea85a34327daef98ce7f8199dc445e6b4c02e232
ilhamhafidz404 <xxspanzie@gmail.com>
2025-01-05 22:29:45 +0700

    Footer

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -1,10 +1,14 @@
 import {
+    IconBrandFacebook,
+    IconBrandInstagram,
     IconCalendar,
     IconCalendar2Fill,
     IconCalendarFill,
     IconChevronRight,
     IconEye,
     IconHeart,
+    IconHome1,
+    IconHome1Fill,
     IconLocation,
     IconLocationFill,
     IconMessages,
@@ -606,7 +610,7 @@ function Layout() {
                 </section>
             </section>
 
-            <section className="bg-red-500 py-20 text-center mt-20 mx-auto md:px-10 px-5">
+            <section className="bg-gradient-to-r from-red-500 to-red-600 py-20 text-center mt-20 mx-auto md:px-10 px-5">
                 <p className="text-white font-semibold lg:text-3xl md:text-2xl">
                     “Keberagaman suku dan budaya Indonesia ini menjadi kekayaan{" "}
                     <br /> besar bangsa sekaligus juga kekuatan besar,{" "}
@@ -620,11 +624,42 @@ function Layout() {
                 </small>
             </section>
 
-            <footer className="py-5 text-center text-gray-100">
-                <h3 className="text-3xl font-semibold mb-5">
+            <footer className="pt-16 pb-5 text-center text-gray-100">
+                <span className="size-10 bg-red-500 inline-flex items-center justify-center rounded-full">
+                    <IconHome1Fill className="size-5" />
+                </span>
+
+                <h3 className="text-3xl font-semibold mb-5 mt-2">
                     Sanggar Nusantara
                 </h3>
-                <p>Copyright by ALOPE</p>
+                <ul className="flex justify-center gap-6 items-center text-white mb-5">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Gallery</li>
+                </ul>
+
+                <div className="inline-flex gap-3 my-3">
+                    <span
+                        className="bg-[#f09433] flex items-center justify-center size-10 rounded-full"
+                        style={{
+                            background:
+                                "-moz-linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%)",
+                            background:
+                                "-webkit-linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)",
+                            background:
+                                "linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)",
+                        }}
+                    >
+                        <IconBrandInstagram className="size-6" />
+                    </span>
+                    <span className="bg-gradient-to-t from-[#015ddd] to-[#19aeff] flex items-center justify-center size-10 rounded-full">
+                        <IconBrandFacebook className="size-6" />
+                    </span>
+                </div>
+
+                <hr className="w-5/6 border-t border-gray-700 mx-auto mt-5" />
+
+                <p className="mt-7">Copyright &copy; by ALOPE</p>
             </footer>
         </>
     );
