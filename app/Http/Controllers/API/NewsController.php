74b547814c8cf6930416be9c6533c45e544dff92
ilhamhafidz404 <xxspanzie@gmail.com>
2025-01-07 14:13:56 +0700

    API Update News

--- app/Http/Controllers/API/NewsController.php
+++ app/Http/Controllers/API/NewsController.php
@@ -52,17 +52,50 @@ class NewsController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $slug)
     {
-        //
+        $news = News::whereSlug($slug)->first();
+
+        if (!$news) {
+            return response()->json(['message' => 'News not found'], 404);
+        }
+
+        return new NewsResource($news);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $slug)
     {
-        //
+        $news = News::whereSlug($slug)->first();
+
+        if (!$news) {
+            return response()->json(['message' => 'News not found'], 404);
+        }
+
+        $validated = $request->validate([
+            'title' => 'sometimes|string|max:255',
+            // 'slug' => 'sometimes|string|unique:news,slug,' . $id,
+            'cover' => 'sometimes',
+            'description' => 'sometimes|string',
+            'body' => 'sometimes|string',
+        ]);
+
+        // Buat slug awal dari title
+        $slug = Str::slug($validated['title']);
+
+        // Tambahkan string acak 3 digit ke slug
+        $randomString = Str::random(3); // 3 karakter acak
+        $slugWithRandom = $slug . '-' . $randomString;
+
+
+        // Tetapkan slug yang unik ke data yang divalidasi
+        $validated['slug'] = $slugWithRandom;
+
+        $news->update($request->all());
+
+        return response()->json($news, 200);
     }
 
     /**
