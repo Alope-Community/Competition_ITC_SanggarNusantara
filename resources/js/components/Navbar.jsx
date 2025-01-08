f99f67421e426403f4a8059290e9e3b4c2e34e00
ilhamhafidz404 <xxspanzie@gmail.com>
2025-01-08 14:35:50 +0700

    Hamburger Menu Navbar in Small Device

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -21,6 +21,10 @@ export default function NavbarComponent() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const toggleModal = () => setIsModalOpen(!isModalOpen);
 
+    // Hamburger Menu
+    const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+    const toggleHamburger = () => setIsHamburgerOpen(!isHamburgerOpen);
+
     // Fungsi untuk menentukan apakah link aktif
     const isActive = (path) => url.startsWith(path);
 
@@ -172,12 +176,136 @@ export default function NavbarComponent() {
                         )}
                     </div>
                     <div className="md:hidden block">
-                        <button>
+                        <button onClick={toggleHamburger}>
                             <IconBarsThree className="text-white size-5" />
                         </button>
                     </div>
                 </div>
             </nav>
+
+            {isHamburgerOpen && (
+                <div className="md:hidden fixed top-16 z-50 left-0 right-0 backdrop-blur border-b border-red-500">
+                    <ul className="flex flex-col gap-4 items-center py-5 text-white">
+                        <li>
+                            <Link
+                                href="/"
+                                className={url == "/" ? "font-bold" : ""}
+                                onClick={toggleHamburger}
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/news"
+                                className={isActive("/news") ? "font-bold" : ""}
+                                onClick={toggleHamburger}
+                            >
+                                Berita
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/events"
+                                className={
+                                    isActive("/events") ? "font-bold" : ""
+                                }
+                                onClick={toggleHamburger}
+                            >
+                                Event
+                            </Link>
+                        </li>
+                        {auth.user ? (
+                            <div className="relative">
+                                <button
+                                    onClick={toggleDropdown}
+                                    className="flex items-center gap-2 bg-gradient-to-r from-red-500 to-red-600 hover:bg-gradient-to-l px-4 py-2 rounded-full text-white text-sm"
+                                >
+                                    <IconCirclePersonFill className="size-5" />
+                                    <span>{auth.user.name}</span>
+                                    <IconChevronLgDown
+                                        className={`w-3 h-3 transition-transform ${
+                                            isDropdownOpen ? "rotate-180" : ""
+                                        }`}
+                                    />
+                                </button>
+
+                                {isDropdownOpen && (
+                                    <div className="absolute left-1/2 -translate-x-1/2 mt-2 w-56 bg-black shadow-md rounded-md z-10 pt-3">
+                                        <ul className="py-1">
+                                            <li className="text-gray-200 text-sm font-semibold px-5">
+                                                <p className="text-xs text-gray-300">
+                                                    Email:
+                                                </p>
+                                                {auth.user.email}
+                                                {/*  */}
+                                                {auth.user
+                                                    .email_subscribed_at ? (
+                                                    <div className="mt-2 text-gray-200 text-sm font-semibold">
+                                                        <p className="text-xs text-gray-300">
+                                                            Berlangganan Sejak :
+                                                        </p>
+                                                        <p>
+                                                            {formatDate(
+                                                                auth.user
+                                                                    .email_subscribed_at
+                                                            )}
+                                                        </p>
+                                                    </div>
+                                                ) : (
+                                                    ""
+                                                )}
+                                            </li>
+                                            {auth.user.email_subscribed_at ? (
+                                                <li className="mt-3">
+                                                    <button
+                                                        onClick={() =>
+                                                            toggleModal()
+                                                        }
+                                                        className="w-full text-left block px-4 py-1.5 text-sm text-gray-200 hover:bg-red-500"
+                                                    >
+                                                        Berhenti Langganan
+                                                    </button>
+                                                </li>
+                                            ) : (
+                                                <li className="mt-3">
+                                                    <button
+                                                        onClick={() =>
+                                                            toggleModal()
+                                                        }
+                                                        className="w-full text-left block px-4 py-1.5 text-sm text-gray-200 hover:bg-red-500"
+                                                    >
+                                                        Berlangganan
+                                                    </button>
+                                                </li>
+                                            )}
+                                            <li>
+                                                <button
+                                                    onClick={() =>
+                                                        handleLogout()
+                                                    }
+                                                    className="w-full text-left block px-4 py-1.5 text-sm text-gray-200 hover:bg-red-500"
+                                                >
+                                                    Logout
+                                                </button>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                )}
+                            </div>
+                        ) : (
+                            <Link
+                                href="/login"
+                                className={
+                                    "bg-gradient-to-r from-red-500 to-red-600 hover:bg-gradient-to-l px-6 py-2 rounded-full text-white text-sm"
+                                }
+                            >
+                                Login
+                            </Link>
+                        )}
+                    </ul>
+                </div>
+            )}
 
             {isModalOpen && (
                 <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
